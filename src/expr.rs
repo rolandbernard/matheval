91d458118a3dd81adf7bca347bb8ec756e9f0f5c
Roland Bernard <rolli.bernard@gmail.com>
2022-02-22 17:20:53 +0100

    First ideas

---
+++ src/expr.rs
@@ -0,0 +1,28 @@
+
+
+// use std::collections::HashMap;
+
+pub mod value;
+
+
+
+// pub type ExprId = usize;
+
+// pub enum ExprTree {
+//     Constant(Value),
+//     Sum(HashMap<ExprId, Value>),
+//     Product(HashMap<ExprId, Value>),
+//     Function(String, ExprId),
+// }
+
+// pub struct Context {
+//     expr: HashMap<ExprTree, ExprId>,
+// }
+
+// pub struct Expr<'a> {
+//     context: &'a Context,
+// }
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 
+use num::Num;
+
 fn main() {
-    println!("Hello, world!");
+    let v = matheval::expr::value::Value::from_str_radix("123/1", 10).unwrap();
+    let u = v.clone() + v.clone();
+    println!("Hello, world! {}", u.to_f64());
 }
 
