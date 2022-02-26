2e078223dbf48e6d58b31fa46de08dabe5807169
Roland Bernard <rolli.bernard@gmail.com>
2022-02-26 17:27:56 +0100

    Changed constants to floats

--- src/number/constants.rs
+++ src/number/constants.rs
@@ -9,17 +9,11 @@ impl Number {
     }
 
     pub fn pi() -> Number {
-        Number::Rational(BigRational::new(
-            BigInt::from_u64(2646693125139304345).unwrap(), 
-            BigInt::from_u64(842468587426513207).unwrap()
-        ))
+        Number::Float(3.1415926535897932384626433832795)
     }
 
     pub fn e() -> Number {
-        Number::Rational(BigRational::new(
-            BigInt::from_u128(27182818284590452353602874713527).unwrap(), 
-            BigInt::from_u128(1000000000000000000000000000000).unwrap()
-        ))
+        Number::Float(2.7182818284590452353602874713527)
     }
 }
 
