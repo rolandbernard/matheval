04bc9e69c0de279aa4eb5331a316b81a584afbc7
Roland Bernard <rolli.bernard@gmail.com>
2022-02-27 19:03:31 +0100

    Added testing for parsing literals

--- tests/parser.rs
+++ tests/parser.rs
@@ -3,8 +3,8 @@ use matheval::Expr;
 
 #[test]
 fn parse_simple_integer_literal() {
-    let parsed = Expr::parse("42").expect("Failed to parse simple integer literal");
-    assert_eq!(Expr::Literal("42".to_owned()), parsed);
+    let parsed = Expr::parse("98765432109876543210").expect("Failed to parse simple integer literal");
+    assert_eq!(Expr::Literal("98765432109876543210".to_owned()), parsed);
 }
 
 #[test]
@@ -15,23 +15,81 @@ fn parse_long_integer_literal() {
 }
 
 #[test]
-fn parse_simple_binary_literal() {
-    let parsed = Expr::parse("0b1010100011110")
+fn parse_binary_integer_literal() {
+    let parsed = Expr::parse("0b1010")
         .expect("Failed to parse binary integer literal");
-    assert_eq!(Expr::Literal("0b1010100011110".to_owned()), parsed);
+    assert_eq!(Expr::Literal("0b1010".to_owned()), parsed);
 }
 
 #[test]
-fn parse_simple_octal_literal() {
-    let parsed = Expr::parse("0o123765400")
+fn parse_octal_integer_literal() {
+    let parsed = Expr::parse("0o7654321076543210")
         .expect("Failed to parse octal integer literal");
-    assert_eq!(Expr::Literal("0o123765400".to_owned()), parsed);
+    assert_eq!(Expr::Literal("0o7654321076543210".to_owned()), parsed);
 }
 
 #[test]
-fn parse_simple_hex_literal() {
-    let parsed = Expr::parse("0x24680afbcde13579")
+fn parse_hex_integer_literal() {
+    let parsed = Expr::parse("0xfedcba9876543210fedcba9876543210")
         .expect("Failed to parse hex integer literal");
-    assert_eq!(Expr::Literal("0x24680afbcde13579".to_owned()), parsed);
+    assert_eq!(Expr::Literal("0xfedcba9876543210fedcba9876543210".to_owned()), parsed);
 }
+
+#[test]
+fn parse_simple_nonint_literal() {
+    let parsed = Expr::parse("9876543210.0123456789")
+        .expect("Failed to parse simple non-integer literal");
+    assert_eq!(Expr::Literal("9876543210.0123456789".to_owned()), parsed);
+    assert!(Expr::parse("98765432109876543210.").is_err());
+    assert!(Expr::parse(".98765432109876543210").is_err());
+}
+
+#[test]
+fn parse_binary_nonint_literal() {
+    let parsed = Expr::parse("0b1010.0101")
+        .expect("Failed to parse binary non-integer literal");
+    assert_eq!(Expr::Literal("0b1010.0101".to_owned()), parsed);
+    assert!(Expr::parse("0b1010.").is_err());
+}
+
+#[test]
+fn parse_octal_nonint_literal() {
+    let parsed = Expr::parse("0o76543210.01234567")
+        .expect("Failed to parse octal non-integer literal");
+    assert_eq!(Expr::Literal("0o76543210.01234567".to_owned()), parsed);
+    assert!(Expr::parse("0o76543210.").is_err());
+}
+
+#[test]
+fn parse_hex_nonint_literal() {
+    let parsed = Expr::parse("0xfedcba9876543210.0123456789abcdef")
+        .expect("Failed to parse hex non-integer literal");
+    assert_eq!(Expr::Literal("0xfedcba9876543210.0123456789abcdef".to_owned()), parsed);
+    assert!(Expr::parse("0xfedcba9876543210.").is_err());
+}
+
+#[test]
+fn parse_simple_exponent_literal() {
+    let parsed = Expr::parse("9876543210.0123456789e-0123456789")
+        .expect("Failed to parse simple non-integer literal");
+    assert_eq!(Expr::Literal("9876543210.0123456789e-0123456789".to_owned()), parsed);
+    let parsed = Expr::parse("9876543210.0123456789e+0123456789")
+        .expect("Failed to parse simple non-integer literal");
+    assert_eq!(Expr::Literal("9876543210.0123456789e+0123456789".to_owned()), parsed);
+    let parsed = Expr::parse("9876543210.0123456789e0123456789")
+        .expect("Failed to parse simple non-integer literal");
+    assert_eq!(Expr::Literal("9876543210.0123456789e0123456789".to_owned()), parsed);
+    let parsed = Expr::parse("98765432100123456789e-0123456789")
+        .expect("Failed to parse simple non-integer literal");
+    assert_eq!(Expr::Literal("98765432100123456789e-0123456789".to_owned()), parsed);
+    let parsed = Expr::parse("98765432100123456789e+0123456789")
+        .expect("Failed to parse simple non-integer literal");
+    assert_eq!(Expr::Literal("98765432100123456789e+0123456789".to_owned()), parsed);
+    let parsed = Expr::parse("98765432100123456789e0123456789")
+        .expect("Failed to parse simple non-integer literal");
+    assert_eq!(Expr::Literal("98765432100123456789e0123456789".to_owned()), parsed);
+}
+
+
+
 
