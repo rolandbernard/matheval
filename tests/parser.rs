e49f2a76cf99dbf6ce55317ac2b30c82695c9489
Roland Bernard <rolli.bernard@gmail.com>
2022-02-27 19:36:40 +0100

    Added tests for parsing expressions

--- tests/parser.rs
+++ tests/parser.rs
@@ -90,6 +90,307 @@ fn parse_simple_exponent_literal() {
     assert_eq!(Expr::Literal("98765432100123456789e0123456789".to_owned()), parsed);
 }
 
+#[test]
+fn parse_variable() {
+    let parsed = Expr::parse("abcxyz").expect("Failed to parse variable");
+    assert_eq!(Expr::Variable("abcxyz".to_owned()), parsed);
+    let parsed = Expr::parse("__abc__xyz").expect("Failed to parse variable");
+    assert_eq!(Expr::Variable("__abc__xyz".to_owned()), parsed);
+    let parsed = Expr::parse("abc123890").expect("Failed to parse variable");
+    assert_eq!(Expr::Variable("abc123890".to_owned()), parsed);
+    let parsed = Expr::parse("abc123xyz").expect("Failed to parse variable");
+    assert_eq!(Expr::Variable("abc123xyz".to_owned()), parsed);
+}
 
+#[test]
+fn parse_simple_add() {
+    let parsed = Expr::parse("a1 + a2").expect("Failed to parse simple add");
+    assert_eq!(Expr::Add(
+        Box::new(Expr::Variable("a1".to_owned())),
+        Box::new(Expr::Variable("a2".to_owned()))
+    ), parsed);
+}
 
+#[test]
+fn parse_simple_sub() {
+    let parsed = Expr::parse("a1 - a2").expect("Failed to parse simple sub");
+    assert_eq!(Expr::Sub(
+        Box::new(Expr::Variable("a1".to_owned())),
+        Box::new(Expr::Variable("a2".to_owned()))
+    ), parsed);
+}
+
+#[test]
+fn parse_mixed_add_sub() {
+    let parsed = Expr::parse("a1 + a2 - a3 + a4").expect("Failed to parse mixed add/sub");
+    assert_eq!(Expr::Add(
+        Box::new(Expr::Sub(
+            Box::new(Expr::Add(
+                Box::new(Expr::Variable("a1".to_owned())),
+                Box::new(Expr::Variable("a2".to_owned()))
+            )),
+            Box::new(Expr::Variable("a3".to_owned()))
+        )),
+        Box::new(Expr::Variable("a4".to_owned()))
+    ), parsed);
+}
+
+#[test]
+fn parse_simple_mul() {
+    let parsed = Expr::parse("a1 * a2").expect("Failed to parse simple mul");
+    assert_eq!(Expr::Mul(
+        Box::new(Expr::Variable("a1".to_owned())),
+        Box::new(Expr::Variable("a2".to_owned()))
+    ), parsed);
+}
+
+#[test]
+fn parse_simple_div() {
+    let parsed = Expr::parse("a1 / a2").expect("Failed to parse simple div");
+    assert_eq!(Expr::Div(
+        Box::new(Expr::Variable("a1".to_owned())),
+        Box::new(Expr::Variable("a2".to_owned()))
+    ), parsed);
+}
+
+#[test]
+fn parse_mixed_mul_div() {
+    let parsed = Expr::parse("a1 * a2 / a3 * a4").expect("Failed to parse mixed mul/div");
+    assert_eq!(Expr::Mul(
+        Box::new(Expr::Div(
+            Box::new(Expr::Mul(
+                Box::new(Expr::Variable("a1".to_owned())),
+                Box::new(Expr::Variable("a2".to_owned()))
+            )),
+            Box::new(Expr::Variable("a3".to_owned()))
+        )),
+        Box::new(Expr::Variable("a4".to_owned()))
+    ), parsed);
+}
+
+#[test]
+fn parse_simple_pow() {
+    let parsed = Expr::parse("a1 ^ a2").expect("Failed to parse simple pow");
+    assert_eq!(Expr::Pow(
+        Box::new(Expr::Variable("a1".to_owned())),
+        Box::new(Expr::Variable("a2".to_owned()))
+    ), parsed);
+}
+
+#[test]
+fn parse_mixed_pow() {
+    let parsed = Expr::parse("a1 ^ a2 ^ a3 ^ a4").expect("Failed to parse mixed pow");
+    assert_eq!(Expr::Pow(
+        Box::new(Expr::Variable("a1".to_owned())),
+        Box::new(Expr::Pow(
+            Box::new(Expr::Variable("a2".to_owned())),
+            Box::new(Expr::Pow(
+                Box::new(Expr::Variable("a3".to_owned())),
+                Box::new(Expr::Variable("a4".to_owned()))
+            ))
+        ))
+    ), parsed);
+}
+
+#[test]
+fn parse_simple_neg() {
+    let parsed = Expr::parse("--a1").expect("Failed to parse simple neg");
+    assert_eq!(Expr::Neg(Box::new(Expr::Neg(
+        Box::new(Expr::Variable("a1".to_owned()))
+    ))), parsed);
+}
+
+#[test]
+fn parse_simple_function() {
+    let parsed = Expr::parse("func(a1)").expect("Failed to parse simple function");
+    assert_eq!(Expr::Function("func".to_owned(), vec![Expr::Variable("a1".to_owned())]), parsed);
+}
+
+#[test]
+fn parse_multi_variable_function() {
+    let parsed = Expr::parse("func(a1, a2, a3, a4, a5)")
+        .expect("Failed to parse multi-variable function");
+    assert_eq!(Expr::Function("func".to_owned(), vec![
+        Expr::Variable("a1".to_owned()),
+        Expr::Variable("a2".to_owned()),
+        Expr::Variable("a3".to_owned()),
+        Expr::Variable("a4".to_owned()),
+        Expr::Variable("a5".to_owned())
+    ]), parsed);
+}
+
+#[test]
+fn parse_nested_function() {
+    let parsed = Expr::parse("func1(a1, func2(func3(a2), func4(a3, a4), a5))")
+        .expect("Failed to parse nested function");
+    assert_eq!(Expr::Function("func1".to_owned(), vec![
+        Expr::Variable("a1".to_owned()),
+        Expr::Function("func2".to_owned(), vec![
+            Expr::Function("func3".to_owned(), vec![
+                Expr::Variable("a2".to_owned())
+            ]),
+            Expr::Function("func4".to_owned(), vec![
+                Expr::Variable("a3".to_owned()),
+                Expr::Variable("a4".to_owned()),
+            ]),
+            Expr::Variable("a5".to_owned())
+        ])
+    ]), parsed);
+}
+
+#[test]
+fn parse_precedence_add_mul() {
+    let parsed = Expr::parse("a1 * a2 + a3 / a4 - a5 * a6 + a7 / a8")
+        .expect("Failed to parse expression");
+    assert_eq!(Expr::Add(
+        Box::new(Expr::Sub(
+            Box::new(Expr::Add(
+                Box::new(Expr::Mul(
+                    Box::new(Expr::Variable("a1".to_owned())),
+                    Box::new(Expr::Variable("a2".to_owned()))
+                )),
+                Box::new(Expr::Div(
+                    Box::new(Expr::Variable("a3".to_owned())),
+                    Box::new(Expr::Variable("a4".to_owned()))
+                ))
+            )),
+            Box::new(Expr::Mul(
+                Box::new(Expr::Variable("a5".to_owned())),
+                Box::new(Expr::Variable("a6".to_owned()))
+            ))
+        )),
+        Box::new(Expr::Div(
+            Box::new(Expr::Variable("a7".to_owned())),
+            Box::new(Expr::Variable("a8".to_owned()))
+        ))
+    ), parsed);
+}
+
+#[test]
+fn parse_precedence_mul_pow() {
+    let parsed = Expr::parse("a1 ^ a2 * a3 ^ a4 / a5 ^ a6 * a7 ^ a8")
+        .expect("Failed to parse expression");
+    assert_eq!(Expr::Mul(
+        Box::new(Expr::Div(
+            Box::new(Expr::Mul(
+                Box::new(Expr::Pow(
+                    Box::new(Expr::Variable("a1".to_owned())),
+                    Box::new(Expr::Variable("a2".to_owned()))
+                )),
+                Box::new(Expr::Pow(
+                    Box::new(Expr::Variable("a3".to_owned())),
+                    Box::new(Expr::Variable("a4".to_owned()))
+                ))
+            )),
+            Box::new(Expr::Pow(
+                Box::new(Expr::Variable("a5".to_owned())),
+                Box::new(Expr::Variable("a6".to_owned()))
+            ))
+        )),
+        Box::new(Expr::Pow(
+            Box::new(Expr::Variable("a7".to_owned())),
+            Box::new(Expr::Variable("a8".to_owned()))
+        ))
+    ), parsed);
+}
+
+#[test]
+fn parse_precedence_neg_pow() {
+    let parsed = Expr::parse("- a1 ^ - a2 ^ a3 ^ - a4")
+        .expect("Failed to parse expression");
+    assert_eq!(Expr::Pow(
+        Box::new(Expr::Neg(Box::new(Expr::Variable("a1".to_owned())))),
+        Box::new(Expr::Pow(
+            Box::new(Expr::Neg(Box::new(Expr::Variable("a2".to_owned())))),
+            Box::new(Expr::Pow(
+                Box::new(Expr::Variable("a3".to_owned())),
+                Box::new(Expr::Neg(Box::new(Expr::Variable("a4".to_owned()))))
+            )),
+        )),
+    ), parsed);
+}
+
+#[test]
+fn parse_precedence_parens() {
+    let parsed = Expr::parse("a1 * (((a2 + a3) / (a4 - a5) * (a6 + a7)) / a8)")
+        .expect("Failed to parse expression");
+    assert_eq!(Expr::Mul(
+        Box::new(Expr::Variable("a1".to_owned())),
+        Box::new(Expr::Div(
+            Box::new(Expr::Mul(
+                Box::new(Expr::Div(
+                    Box::new(Expr::Add(
+                        Box::new(Expr::Variable("a2".to_owned())),
+                        Box::new(Expr::Variable("a3".to_owned()))
+                    )),
+                    Box::new(Expr::Sub(
+                        Box::new(Expr::Variable("a4".to_owned())),
+                        Box::new(Expr::Variable("a5".to_owned()))
+                    ))
+                )),
+                Box::new(Expr::Add(
+                    Box::new(Expr::Variable("a6".to_owned())),
+                    Box::new(Expr::Variable("a7".to_owned()))
+                )),
+            )),
+            Box::new(Expr::Variable("a8".to_owned()))
+        ))
+    ), parsed);
+}
+
+#[test]
+fn parse_precedence_add_pow() {
+    let parsed = Expr::parse("a1 ^ a2 + a3 ^ a4 - a5 ^ a6 + a7 ^ a8")
+        .expect("Failed to parse expression");
+    assert_eq!(Expr::Add(
+        Box::new(Expr::Sub(
+            Box::new(Expr::Add(
+                Box::new(Expr::Pow(
+                    Box::new(Expr::Variable("a1".to_owned())),
+                    Box::new(Expr::Variable("a2".to_owned()))
+                )),
+                Box::new(Expr::Pow(
+                    Box::new(Expr::Variable("a3".to_owned())),
+                    Box::new(Expr::Variable("a4".to_owned()))
+                ))
+            )),
+            Box::new(Expr::Pow(
+                Box::new(Expr::Variable("a5".to_owned())),
+                Box::new(Expr::Variable("a6".to_owned()))
+            ))
+        )),
+        Box::new(Expr::Pow(
+            Box::new(Expr::Variable("a7".to_owned())),
+            Box::new(Expr::Variable("a8".to_owned()))
+        ))
+    ), parsed);
+}
+
+#[test]
+fn parse_precedence_add_mul_pow() {
+    let parsed = Expr::parse("a1 ^ a2 - a3 ^ a4 / a5 ^ a6 * a7 ^ a8")
+        .expect("Failed to parse expression");
+    assert_eq!(Expr::Sub(
+        Box::new(Expr::Pow(
+            Box::new(Expr::Variable("a1".to_owned())),
+            Box::new(Expr::Variable("a2".to_owned()))
+        )),
+        Box::new(Expr::Mul(
+            Box::new(Expr::Div(
+                Box::new(Expr::Pow(
+                    Box::new(Expr::Variable("a3".to_owned())),
+                    Box::new(Expr::Variable("a4".to_owned()))
+                )),
+                Box::new(Expr::Pow(
+                    Box::new(Expr::Variable("a5".to_owned())),
+                    Box::new(Expr::Variable("a6".to_owned()))
+                ))
+            )),
+            Box::new(Expr::Pow(
+                Box::new(Expr::Variable("a7".to_owned())),
+                Box::new(Expr::Variable("a8".to_owned()))
+            ))
+        )),
+    ), parsed);
+}
 
